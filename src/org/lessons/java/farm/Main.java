c1dc19447bea3328dcc70874606f6b6597de408f
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-08 16:54:14 +0200

    spero sia giusto perche' non posso stampare in console

--- src/org/lessons/java/farm/Main.java
+++ src/org/lessons/java/farm/Main.java
@@ -1,6 +1,13 @@
 package org.lessons.java.farm;
 
 public class Main {
+//    creo i due metodi static per averli a disposizione per animali volanti e che nuotano
+    public static void faiVolare(Volante animale) {
+        animale.vola();
+    }
+    public static void faiNuotare(Nuotante animale) {
+        animale.nuota();
+    }
     public static void main(String[] args) {
 //        creo un array degli animali
         Animal[] farm = new Animal[4];
@@ -11,7 +18,21 @@ public class Main {
 
 //        ciclo gli animali nel mio array e li stampo a schermo
         for (int i = 0; i < farm.length; i++) {
-            System.out.println((i + 1) + "° animale ---");
+            System.out.println((i + 1) + "° animale");
+//            getClass() metodo che restituisce il nome della classe o l'interfaccia rappresentata da un oggetto
+            System.out.println("Classe: " + farm[i].getClass().getSimpleName());
+            System.out.print("Nanna: ");
+            farm[i].dormi();
+            System.out.print("Cibo: ");
+            farm[i].mangia();
+            System.out.print("Verso: ");
+            farm[i].verso();
+            if (farm[i] instanceof Volante){
+//                notare che i metodi static cambiano font per evidenziare la loro natura
+                faiVolare((Volante)farm[i]);
+            }else{
+                faiNuotare((Nuotante)farm[i]);
+            }
         }
     }
 }
